2ad9b2a2e3d4dec02865763097241949d4efd155
Mehmet Koca <mehmetkoca@outlook.com>
2017-12-11 18:20:53 +0300

    first upload

---
+++ src/main/java/com/rm/inventorytracking/InventoryTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.rm.inventorytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rm/inventorytracking/InventoryTrackingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rm.inventorytracking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InventoryTrackingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
