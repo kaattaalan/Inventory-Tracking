e602770625dc5be0c3c5cd6fd1bb662faa5bde65
Mehmet Koca <mehmetkoca@outlook.com>
2017-12-13 19:35:57 +0300

    homepage error fixed

--- src/main/java/com/rm/inventorytracking/domain/User.java
+++ src/main/java/com/rm/inventorytracking/domain/User.java
@@ -6,7 +6,6 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 
 
 import javax.persistence.*;
